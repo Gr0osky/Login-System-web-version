a985ce1de4959153afe11b9cd4b7b568663cc327
Gr0osky <marvelsinmars@gmail.com>
2025-01-16 19:59:04 +0530

    Added Js FUNCTIONS to CreateAccPage
    duh

--- scripts/CreateAcc_page/index.js
+++ scripts/CreateAcc_page/index.js
@@ -1,1 +1,16 @@
-UsernameField = document.getElementById("Username");
+UsernameField = document.getElementById("UserIn");
+PasswordField = document.getElementById("PassIn");
+UsernameDisturbance = document.getElementById("UsernameDistrubance");
+Terms = document.getElementById("check");
+UsernameSpace = document.getElementById("li1");
+UsernameMin = document.getElementById("li2");
+UsernameMax = document.getElementById("li3");
+
+// Will work tommorow :(
+
+function checkForUsrField(){
+    if(" " in UsernameField.value){
+        UsernameSpace.style.color = "red";
+    }
+}
+UsernameField.addEventListener('input', checkForUsrField);
