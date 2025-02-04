11beadb8b89ae0ee884aefa7ecb61b6073653939
Gr0osky <marvelsinmars@gmail.com>
2025-02-04 19:50:25 +0530

    Login Page started
    Started work on login page

--- scripts/Home_page/index.js
+++ scripts/Home_page/index.js
@@ -1,5 +1,10 @@
-CreateAcc = document.getElementById("CreateAcc");
+const CreateAcc = document.getElementById("CreateAcc");
+const Login = document.getElementById("login")
 
 CreateAcc.onclick = function(){
     window.location.href = '../CreateAcc_page/index.html'
+}
+
+Login.onclick = function(){
+    window.location.href = "../Login_page/index.html"
 }
